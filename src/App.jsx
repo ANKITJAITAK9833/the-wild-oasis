175e2312a377c1c3235fa2b681fa8d1dc9e9ac8f
Ankit Jaitak <jaitak9833ankit@gmail.com>
2025-12-09 09:14:31 +0530

    Updated styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-function App(){
+function App() {
   return (
-    <div>
-      The wild oasis
-    </div>
+    <>
+      <GlobalStyles />
+      <div>
+       <Row type="vertical"> 
+        <Row type="horizontal">
+          <Heading>
+            The wild oasis
+          </Heading>
+          <div>
+
+            <Heading as="h3">
+              Buttons
+            </Heading>
+            <Button   onClick={() => alert("User wants to check in")}>Check in</Button>
+            <Button variation="danger" size="large"  onClick={() => alert("User wants to check out")}>Check out</Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <Heading as="h3"> Form </Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+        </Row>
+      </div>
+    </>
   )
 }
 
