af323fe9c9ba3bd97058440054ac6dd48916b54e
Ankit Jaitak <jaitak9833ankit@gmail.com>
2025-12-13 15:32:24 +0530

    Reusable context menu - most confusing

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,17 +6,9 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Modal from "../../ui/Modal";
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
 
 const Img = styled.img`
   display: block;
@@ -46,48 +38,58 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { id: cabinId, name, maxCapacity, regularPrice, discount, image , description} = cabin;
-  const {isCreating, createCabin} = useCreateCabin();
-  function handleDuplicate(){
-     createCabin({
-      name : `Copy of ${name}`,
+  const { id: cabinId, name, maxCapacity, regularPrice, discount, image, description } = cabin;
+  const { isCreating, createCabin } = useCreateCabin();
+  console.log("isCreating", isCreating);
+  function handleDuplicate() {
+    createCabin({
+      name: `Copy of ${name}`,
       maxCapacity,
       regularPrice,
       discount,
-      image,description
-     })
+      image, description
+    })
   }
-  const {isPending: isDeleting, deleteCabin} = useDeleteCabin();
+  const { isPending: isDeleting, deleteCabin } = useDeleteCabin();
   return (
     <>
-    <TableRow role="row">
-      <Img src={image} />
-      <Cabin>{name}</Cabin>
-      <div>Fits up to {maxCapacity} guests</div>
-      <Price>{formatCurrency(regularPrice)}</Price>
-      {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
-      <div>
-        <button disabled={isCreating} onClick={()=>handleDuplicate()}><HiSquare2Stack/></button>
-        <Modal>
-          <Modal.Open opens="edit">
-            <button ><HiPencil/></button>
-          </Modal.Open>
-          <Modal.Window name="edit">
-            <CreateCabinForm cabinToEdit={cabin} />
-          </Modal.Window>
-        </Modal>
-        <Modal>
-          <Modal.Open opens="delete">
+      <Table.Row role="row">
+        <Img src={image} />
+        <Cabin>{name}</Cabin>
+        <div>Fits up to {maxCapacity} guests</div>
+        <Price>{formatCurrency(regularPrice)}</Price>
+        {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
+        <div>
+          <Modal>
 
-            <button><HiTrash/></button>
-          </Modal.Open>
-          <Modal.Window name="delete">
-            <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}></ConfirmDelete>
-          </Modal.Window>
-        </Modal>
-      </div>
-    </TableRow>
-    </> 
+            <Menus.Menu>
+              <Menus.Toggle id={cabinId} />
+              <Menus.List id={cabinId}>
+                <Menus.Button onClick={() => handleDuplicate()} icon={<HiSquare2Stack />}>Duplicate</Menus.Button>
+
+                <Modal.Open opens="edit">
+                  <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+
+                </Modal.Open>
+
+            <Modal.Open opens="delete">
+
+                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+            </Modal.Open>
+              </Menus.List>
+            </Menus.Menu>
+
+            <Modal.Window name="edit">
+              <CreateCabinForm cabinToEdit={cabin} />
+            </Modal.Window>
+          
+            <Modal.Window name="delete">
+              <ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}></ConfirmDelete>
+            </Modal.Window>
+          </Modal>
+        </div>
+      </Table.Row>
+    </>
   )
 }
 
